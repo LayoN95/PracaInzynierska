3affcf4fdad62c1d31168c524148159d3e0ff638
LayoN95 <mborowczyk95@gmail.com>
2019-06-22 16:42:40 +0200

    Add DHT11 Sensor

--- Server/api/routes/index.js
+++ Server/api/routes/index.js
@@ -9,16 +9,16 @@ var test = require('./test.json');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  res.render('index', { title: 'Express' });
-  console.log(ds18b20.temp);
-  res.write(ds18b20.temp);
+  res.status(200).json({
+    message: ds18b20.temp
+  });
 });
 
 router.get('/dht11', function(req, res, next) {
 
-  res.render('index', { title: 'Express' });
-  console.log(dht11.temp);
-  res.write(dht11.temp);
+ res.status(200).json({
+   message: dht11.temp
+ });
 });
 
 module.exports = router;
