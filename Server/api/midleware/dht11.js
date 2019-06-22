ff1727b3995d8cf45038f8d8fec26bccfa490cd4
LayoN95 <mborowczyk95@gmail.com>
2019-06-22 17:14:35 +0200

    Add Database

--- Server/api/midleware/dht11.js
+++ Server/api/midleware/dht11.js
@@ -1,4 +1,5 @@
 const sensor = require('node-dht-sensor');
+const dht11Schema = require('../models/dht11');
 
 var temp = 0;
 var humid = 0;
@@ -7,6 +8,12 @@ setInterval(function () {
 
 sensor.read(11, 26, function(err, temperature, humidity) {
     if (!err) {
+        const dht11Schema = dht11Schema({
+            _id: mongoose.Types.ObjectId(),
+            temperature: temperature
+        });
+        dht11Schema.save();
+
         module.exports.temp = temperature;
         module.exports.humid = humidity;
         console.log('temp: ' + temperature.toFixed(1) + '°C, ' +
