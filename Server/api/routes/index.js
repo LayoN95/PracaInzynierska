4ceb909e7f0d4d7d2b27857cc63d4453e34f3fb1
LayoN95 <mborowczyk95@gmail.com>
2019-06-22 17:27:34 +0200

    Add Database

--- Server/api/routes/index.js
+++ Server/api/routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose');
+const DHT11 = require('../models/dht11');
 
 const ds18b20 = require('../midleware/ds18b20');
 const dht11 = require('../midleware/dht11');
@@ -15,10 +17,33 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/dht11', function(req, res, next) {
-
- res.status(200).json({
+  DHT11
+  .find()
+  .select('_id temperature date')
+  .exec()
+  .then(docs => {
+    res.status(200).json({
+      count: docs.length,
+      orders: docs.map(doc => {
+        return {
+          _id: doc.id,
+          temperature: doc.temperature,
+          date: doc.date,
+          request: {
+            type: 'GET'
+          }
+        }
+      })
+    });
+  })
+  .catch(err => {
+    res.status(500).json({
+      error: err
+    });
+  });
+ /*res.status(200).json({
    message: ("Wilgotność: " + dht11.humid + " Temperatura: " + dht11.temp)
- });
+ });*/
 });
 
 module.exports = router;
