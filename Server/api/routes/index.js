28f1f6324ae1389c5b3bf97d160dcb09dc1b421b
LayoN95 <mborowczyk95@gmail.com>
2019-06-23 17:01:03 +0200

    Update

--- Server/api/routes/index.js
+++ Server/api/routes/index.js
@@ -4,19 +4,19 @@ const mongoose = require('mongoose');
 const DHT11 = require('../models/dht11');
 
 const ds18b20 = require('../midleware/ds18b20');
-const dht11 = require('../midleware/dht11');
+//const dht11 = require('../midleware/dht11');
 
 var test = require('./test.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
-  res.status(200).json({
+  res.send(test);
+  /*res.status(200).json({
     message: ds18b20.temp
-  });
+  });*/
 });
 
-router.get('/dht11', function(req, res, next) {
+/*router.get('/dht11', function(req, res, next) {
   DHT11
   .find()
   .select('_id temperature date')
@@ -40,7 +40,7 @@ router.get('/dht11', function(req, res, next) {
   });
  /*res.status(200).json({
    message: ("Wilgotność: " + dht11.humid + " Temperatura: " + dht11.temp)
- });*/
-});
+ });
+});*/
 
 module.exports = router;
