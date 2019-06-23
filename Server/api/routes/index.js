33fabf9080e54688254e0c550614a394d834e9f1
LayoN95 <mborowczyk95@gmail.com>
2019-06-23 17:45:35 +0200

    Update

--- Server/api/routes/index.js
+++ Server/api/routes/index.js
@@ -11,7 +11,19 @@ var test = require('./test.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  DS18B20.find().sort({_id: -1 }).limit(10)
+  DS18B20.find().sort({_id: -1 }).limit(10)  .exec()
+  .then(docs => {
+    res.status(200).json({
+      count: docs.length,
+      ds18b20: docs.map(doc => {
+        return {
+          _id: doc.id,
+          temperature: doc.temperature,
+          date: doc.date
+        }
+      })
+    });
+  })
   /*.findOne()
   .select('_id temperature date')
   .exec()
