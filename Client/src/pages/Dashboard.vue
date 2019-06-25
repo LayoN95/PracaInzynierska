dbacb537e1bf00d30b0c6375b9b49d57da4ba7cc
LayoN95 <mborowczyk95@gmail.com>
2019-06-25 17:43:15 +0200

    Update

--- Client/src/pages/Dashboard.vue
+++ Client/src/pages/Dashboard.vue
@@ -138,9 +138,9 @@
 
   import axios from 'axios';
 
-  var table = [[90, 70, 90, 70, 85, 60, 75, 60, 90, 80, 110, 100],
-            [80, 120, 105, 110, 95, 105, 90, 100, 80, 95, 70, 120],
-            [60, 80, 65, 130, 80, 105, 90, 130, 70, 115, 60, 130]];
+  var table = [[90, 70, 90, 70, 85, 60, 75, 60, 90, 80, 110, 100, 200],
+            [80, 120, 105, 110, 95, 105, 90, 100, 80, 95, 70, 120, 200],
+            [60, 80, 65, 130, 80, 105, 90, 130, 70, 115, 60, 130, 200]];
   export default {
     components: {
       LineChart,
@@ -257,7 +257,7 @@
             pointRadius: 4,
             data: this.bigLineChart.allData[index]
           }],
-          labels: ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
+          labels: ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC', '+1'],
         }
         this.$refs.bigChart.updateGradients(chartData);
         this.bigLineChart.chartData = chartData;
@@ -271,8 +271,9 @@
         }
       })
       .then((response) => {
-        console.log("Odpowiedz: " + response.data.ds18b20);
-        this.User = response.data.ds18b20;
+        //console.log("Odpowiedz: " + response.data.ds18b20);
+        //this.User = response.data.ds18b20;
+        thus.User = JSON.parse(response);
       })
       .catch((error) => {
         console.log(error);
