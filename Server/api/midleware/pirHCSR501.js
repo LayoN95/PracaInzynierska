536d18a4f3029505816bad35b93f22af6c06f046
LayoN95 <mborowczyk95@gmail.com>
2019-07-02 20:36:51 +0200

    update

--- Server/api/midleware/pirHCSR501.js
+++ Server/api/midleware/pirHCSR501.js
@@ -5,8 +5,19 @@ var count = 0;
 
 
     pir.watch(function (err, value) {
+        if (err) exit(err);
+        console.log(value ? 'Ktos tu jest!' : ' Juz Nie!');
+
         count++;
         console.log('Intruder detected' + count);
 
     });
 
+    function exit(err) {
+        if (err) console.log('wystapil blad: ' + err);
+        pir.unexport();
+        console.log('Do zobaczenia!');
+        process.exit();
+    }
+
+    process.on('SIGINT', exit);
