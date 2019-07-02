5c7d66010e4add5d39c367e4f224e1f0372d375c
LayoN95 <mborowczyk95@gmail.com>
2019-07-02 21:01:07 +0200

    update

--- Server/api/midleware/pirHCSR501.js
+++ Server/api/midleware/pirHCSR501.js
@@ -2,7 +2,8 @@ var Gpio = require('onoff').Gpio,
     pir = new Gpio(19, 'in', 'both');
 var count = 0;
 
-
+exports.start = function () {
+    
     pir.watch(function (err, value) {
         if (err) exit(err);
         console.log(value ? 'Ktos tu jest!' : ' Juz Nie!');
@@ -21,3 +22,5 @@ var count = 0;
     }
 
     process.on('SIGINT', exit);
+
+};
