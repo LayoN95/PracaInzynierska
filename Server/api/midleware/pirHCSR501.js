c86014e5fbd1773108392118b115f73c105bf863
LayoN95 <mborowczyk95@gmail.com>
2019-07-02 20:31:48 +0200

    update

--- Server/api/midleware/pirHCSR501.js
+++ Server/api/midleware/pirHCSR501.js
@@ -1,17 +1,14 @@
 var Gpio = require('onoff').Gpio,
-    pir = new Gpio(27, 'in', 'both');
+    pir = new Gpio(19, 'in', 'both');
 var count = 0;
 
 exports.start = function () {
 
 
     pir.watch(function (err, value) {
-        //count++;
-        //console.log('Intruder detected' + count);
+        count++;
+        console.log('Intruder detected' + count);
 
-        socket.emit("intruderValue", {
-            "value": value
-        });
     });
 
 };
