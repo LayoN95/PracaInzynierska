adc69fbee1f8ba35e44230dd037a9aea36ac2b41
LayoN95 <mborowczyk95@gmail.com>
2019-07-03 18:13:22 +0200

    update

--- Server/api/midleware/leds.js
+++ Server/api/midleware/leds.js
@@ -1,19 +1,12 @@
-var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
-var LED = new Gpio(21, 'out'); //use GPIO pin 4, and specify that it is output
-var blinkInterval = setInterval(blinkLED, 250); //run the blinkLED function every 250ms
-
-function blinkLED() { //function to start blinking
-  if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
-    LED.writeSync(1); //set pin state to 1 (turn LED on)
-  } else {
-    LED.writeSync(0); //set pin state to 0 (turn LED off)
-  }
-}
-
-function endBlink() { //function to stop blinking
-  clearInterval(blinkInterval); // Stop blink intervals
-  LED.writeSync(0); // Turn LED off
+const Gpio = require('onoff').Gpio;
+const LED = new Gpio(21, 'out'); // gpio 4 as out
+ 
+LED.writeSync(1); // making the gpio 4 on. Will turn LED on
+ 
+ 
+function switchOff(){
+  LED.writeSync(0); // making the gpio 4 off. Will turn LED off
   LED.unexport(); // Unexport GPIO to free resources
 }
-
-setTimeout(endBlink, 5000); //stop blinking after 5 seconds
+ 
+setTimeout(switchOff, 10000);
