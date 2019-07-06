fb0aa9c679dbaa44cefc9e985164c3d9c8bd8479
LayoN95 <mborowczyk95@gmail.com>
2019-07-06 11:59:18 +0200

    update

--- Client/src/pages/Dashboard.vue
+++ Client/src/pages/Dashboard.vue
@@ -31,7 +31,7 @@
           </template>
           <!-- Szeroki Wykres -->
           <div class="chart-area">
-            <p>{{ /*User.ds18b20[0].temperature*/ Temperature + " " + Dht11 }}</p>
+            <!--<p>{{ /*User.ds18b20[0].temperature*/ Temperature + " " + Dht11 }}</p> -->
             <button v-on:click="turnOn">Turn the lights on!</button>
             <line-chart style="height: 100%"
                         ref="bigChart"
