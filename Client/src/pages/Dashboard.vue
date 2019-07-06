cca9b6bd68b8fb27ae23bf15eb58422c7d6f895d
LayoN95 <mborowczyk95@gmail.com>
2019-07-06 12:07:49 +0200

    update

--- Client/src/pages/Dashboard.vue
+++ Client/src/pages/Dashboard.vue
@@ -33,9 +33,9 @@
           <div class="chart-area">
             <!--<p>{{ /*User.ds18b20[0].temperature*/ Temperature + " " + Dht11 }}</p> -->
 
-            <input v-model="id" placeholder="podaj id">
-            <p>Wybrano id: {{ id }}</p>
-            
+            <input v-model="message" placeholder="podaj id">
+            <p>Wybrano id: {{ message }}</p>
+
             <button v-on:click="turnOn">Turn the lights on!</button>
 
             <button v-on:click="turnOff">Turn the lights off!</button>
