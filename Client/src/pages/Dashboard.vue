0161846c5787bf5ac729a558faa37385c469cc7b
LayoN95 <mborowczyk95@gmail.com>
2019-07-17 18:04:49 +0200

    update

--- Client/src/pages/Dashboard.vue
+++ Client/src/pages/Dashboard.vue
@@ -391,9 +391,35 @@
         this.$refs.bigChart.updateGradients(chartData);
         this.bigLineChart.chartData = chartData;
         this.bigLineChart.activeIndex = index;
-        this.$refs.purpleChart.updateGradients(this.purpleLineChart.chartData);
       }
     },
+    initPurpleChart() {
+      let chartData = {
+            labels: ['JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
+            datasets: [{
+              label: "Temp: ",
+              fill: true,
+              borderColor: config.colors.primary,
+              borderWidth: 2,
+              borderDash: [],
+              borderDashOffset: 0.0,
+              pointBackgroundColor: config.colors.primary,
+              pointBorderColor: 'rgba(255,255,255,0)',
+              pointHoverBackgroundColor: config.colors.primary,
+              pointBorderWidth: 20,
+              pointHoverRadius: 4,
+              pointHoverBorderWidth: 15,
+              pointRadius: 4,
+              data: this.dhtChartTemp,
+            
+          }],
+          labels: this.chartLabels,
+        }
+        this.$refs.purpleChart.updateGradients(chartData);
+        this.purpleLineChart.chartData = chartData;
+
+      },
+
 
        mounted() {
 
