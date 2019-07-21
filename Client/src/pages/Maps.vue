0cf5cf5a5970045b45dc5956597d96c75696e6e3
LayoN95 <mborowczyk95@gmail.com>
2019-07-21 12:28:03 +0200

    update

--- Client/src/pages/Maps.vue
+++ Client/src/pages/Maps.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="card">
     <div class="card-header">
-      <h5 class="title">100 Awesome Nucleo Icons</h5>
-      <p class="category">Handcrafted by our friends from <a href="https://nucleoapp.com/?ref=1712">NucleoApp</a></p>
+      <h5 class="title">Control Devices</h5>
+      <p class="category">A page from you can control yours devices </p>
     </div>
     <div class="card-body all-icons">
       <div class="row">
-        <p>123  </p>
+        <div class="col-lg-4">
+          <p>123</p>
+        </div> 
 
 
 
