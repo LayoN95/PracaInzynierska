c4045717dd8574f6f59bd271d36f366742924672
LayoN95 <mborowczyk95@gmail.com>
2019-07-21 12:32:33 +0200

    update

--- Client/src/pages/Maps.vue
+++ Client/src/pages/Maps.vue
@@ -2,7 +2,12 @@
 <div class="row">
       <div class="col-md-6">
         <card>
-          <p> 123 </p>
+          <h4 slot="header">Light 1</h4>
+        </card>
+      </div>
+      <div class="col-md-6">
+        <card>
+          <h4 slot="header">Light 2</h4>
         </card>
       </div>
 </div>
