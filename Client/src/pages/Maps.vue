7d41a2b0f26816c1dcb8b959717598355af13e34
LayoN95 <mborowczyk95@gmail.com>
2019-07-21 12:16:09 +0200

    update

--- Client/src/pages/Maps.vue
+++ Client/src/pages/Maps.vue
@@ -3,10 +3,8 @@
   <div class="row">
     <div class="col-lg-4">
       <card type="chart">
-          <template slot="header">
-            <h5 class="card-category">{{$t('dashboard.dht11')}}</h5>
-            <h3 class="card-title"><i class="tim-icons icon-bell-55 text-primary "></i> DHT11 Temp + Humid</h3>
-          </template>
+         <p>123 123 123 123 123 123</p>
+         <button v-on:click="turnOn">Turn the lights on!</button>
     </div>
   </div>
 </div>
