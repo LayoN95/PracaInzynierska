1c968354aeaed14feb628c3208327527585cc423
LayoN95 <mborowczyk95@gmail.com>
2019-07-21 12:14:39 +0200

    update

--- Client/src/pages/Maps.vue
+++ Client/src/pages/Maps.vue
@@ -2,7 +2,11 @@
 <div>
   <div class="row">
     <div class="col-lg-4">
-      <p>123</p>
+      <card type="chart">
+          <template slot="header">
+            <h5 class="card-category">{{$t('dashboard.dht11')}}</h5>
+            <h3 class="card-title"><i class="tim-icons icon-bell-55 text-primary "></i> DHT11 Temp + Humid</h3>
+          </template>
     </div>
   </div>
 </div>
