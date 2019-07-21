f6ce1c0bf77ecf7383fb1fa53697139c3be882ef
LayoN95 <mborowczyk95@gmail.com>
2019-07-21 12:16:46 +0200

    update

--- Client/src/pages/Maps.vue
+++ Client/src/pages/Maps.vue
@@ -2,7 +2,6 @@
 <div>
   <div class="row">
     <div class="col-lg-4">
-      <card type="chart">
          <p>123 123 123 123 123 123</p>
          <button v-on:click="turnOn">Turn the lights on!</button>
     </div>
