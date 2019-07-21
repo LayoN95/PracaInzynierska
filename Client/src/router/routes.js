10b2baf36405918914b5073bcdbb9855f86b1487
LayoN95 <mborowczyk95@gmail.com>
2019-07-21 13:39:40 +0200

    update

--- Client/src/router/routes.js
+++ Client/src/router/routes.js
@@ -7,7 +7,7 @@ const Dashboard = () => import(/* webpackChunkName: "dashboard" */"@/pages/Dashb
 const Profile = () => import(/* webpackChunkName: "common" */ "@/pages/Profile.vue");
 const Notifications = () => import(/* webpackChunkName: "common" */"@/pages/Notifications.vue");
 const Icons = () => import(/* webpackChunkName: "common" */ "@/pages/Icons.vue");
-const Lights = () => import(/* webpackChunkName: "common" */ "@/pages/Maps.vue");
+const Maps = () => import(/* webpackChunkName: "common" */ "@/pages/Maps.vue");
 const Typography = () => import(/* webpackChunkName: "common" */ "@/pages/Typography.vue");
 const TableList = () => import(/* webpackChunkName: "common" */ "@/pages/TableList.vue");
 
@@ -38,9 +38,9 @@ const routes = [
         component: Icons
       },
       {
-        path: "maps",
-        name: "maps",
-        component: Lights
+        path: "lights",
+        name: "lights",
+        component: Maps
       },
       {
         path: "typography",
