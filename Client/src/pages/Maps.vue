a54d2cc109b308ca0f5dc2430c98bee16585f9e5
LayoN95 <mborowczyk95@gmail.com>
2019-07-21 12:23:01 +0200

    update

--- Client/src/pages/Maps.vue
+++ Client/src/pages/Maps.vue
@@ -1,14 +1,18 @@
 <template>
-<div>
+<div class="card">
+<div class="card-header">
+      <h5 class="title">Control Devices: </h5>
+      <p class="category">A page from you can control yours devices: <a href="https://nucleoapp.com/?ref=1712">NucleoApp</a></p>
+</div> 
+<div class="card-body all-icons">
   <div class="row">
-    <div class="col-lg-4">
-         <p>123 123 123 123 123 123</p>
-         <button v-on:click="turnOn">Turn the lights on!</button>
-    </div>
+  <div>
+    <p> 123 </p>
   </div>
+  </div>
+
 </div>
-
-
+</div>
 </template>
 <script>
     import axios from 'axios';
