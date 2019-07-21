9b50062f27f15e4fcf12eb2dce4eeac233849d4e
LayoN95 <mborowczyk95@gmail.com>
2019-07-21 12:35:51 +0200

    update

--- Client/src/pages/Maps.vue
+++ Client/src/pages/Maps.vue
@@ -3,11 +3,13 @@
       <div class="col-md-6">
         <card>
           <h4 slot="header">Light 1</h4>
+          <button v-on:click="turnOn">Turn the lights on!</button>
         </card>
       </div>
       <div class="col-md-6">
         <card>
           <h4 slot="header">Light 2</h4>
+          <button v-on:click="turnOn">Turn the lights on!</button>
         </card>
       </div>
 </div>
