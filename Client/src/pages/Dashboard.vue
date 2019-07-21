a86d04a686e8867779a34ee398c53d95073443ca
LayoN95 <mborowczyk95@gmail.com>
2019-07-21 10:59:42 +0200

    update

--- Client/src/pages/Dashboard.vue
+++ Client/src/pages/Dashboard.vue
@@ -59,7 +59,7 @@
         <card type="chart">
           <template slot="header">
             <h5 class="card-category">{{$t('dashboard.dht11')}}</h5>
-            <h3 class="card-title"><i class="tim-icons icon-bell-55 text-primary "></i> 763,215</h3>
+            <!-- <h3 class="card-title"><i class="tim-icons icon-bell-55 text-primary "></i> 763,215</h3> -->
           </template>
           <div class="chart-area">
            <line-chart style="height: 100%"
