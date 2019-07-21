27e5f3ebd14aaccb6330ecc9b7c5d8d67aa1d009
LayoN95 <mborowczyk95@gmail.com>
2019-07-21 12:13:37 +0200

    update

--- Client/src/pages/Maps.vue
+++ Client/src/pages/Maps.vue
@@ -1,71 +1,18 @@
 <template>
-    <card type="plain" title="Google Maps">
-  <div>
-    <h3>123 </h3>
+<div>
+  <div class="row">
+    <div class="col-lg-4">
+      <p>123</p>
+    </div>
   </div>
-    </card>
+</div>
+
+
 </template>
 <script>
-  //import LineChart from './LineChart.js';
     import axios from 'axios';
 
-  
-  export default {
-    components: {
-      LineChart
-    },
-    data () {
-        return {
-        datacollection: null,
-        table: [],
-        dateTable: []
-      }
-    },
-    mounted () {
-      this.fillData(),
-      this.getData()
-    },
-    methods: {
-      fillData () {
-        this.getData(),
-        this.datacollection = {
-          labels: this.dateTable,
-          datasets: [
-            {
-              label: 'Data One',
-              backgroundColor: '#f87979',
-              data: this.table
-            }
-          ]
-        }
-        console.log("filldata: " + this.table);
-      },
-      getData() {
-        axios.get('http://192.168.1.48:3000/', {
-              headers: {
-                'Access-Control-Allow-Origin': '*',
-              }
-        })
-        .then((response) => {
-        var obj = response.data;
-        var x = [];
-        var y = [];
-        for (var i in obj.ds18b20) {
-          x[i] = obj.ds18b20[i].temperature;
-          y[i] = obj.ds18b20[i].date;
-        }
-        console.log(x);
-        this.table = x;
-        this.dateTable = y;
-        console.log(this.table);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-      }
-     
-    }
-  };
+ 
 </script>
 <style>
 </style>
