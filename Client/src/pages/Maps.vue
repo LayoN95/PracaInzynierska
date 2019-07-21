80d26a4877d961d7e226f84db256614fe8366ded
LayoN95 <mborowczyk95@gmail.com>
2019-07-21 12:43:10 +0200

    update

--- Client/src/pages/Maps.vue
+++ Client/src/pages/Maps.vue
@@ -27,7 +27,7 @@
     },
     methods: {
       turnOn: function (event) {
-            axios.post('http://192.168.1.48:3000/leds/' + id + /*this.id*/ + '/1', {
+            axios.post('http://192.168.1.48:3000/leds/' + id /*this.id*/ + '/1', {
               headers: {
                 'Access-Control-Allow-Origin': '*',
               }
@@ -41,7 +41,7 @@
             });
       },
       turnOff: function (event) {
-            axios.post('http://192.168.1.48:3000/leds/' + this.id + '/0', {
+            axios.post('http://192.168.1.48:3000/leds/' + id /*this.id*/+ '/0', {
               headers: {
                 'Access-Control-Allow-Origin': '*',
               }
