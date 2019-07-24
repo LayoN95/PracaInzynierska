a98d3cf862f984b7629a4b8bcf014d26778e60c5
LayoN95 <mborowczyk95@gmail.com>
2019-07-24 18:35:44 +0200

    update

--- Server/api/midleware/schedulde.js
+++ Server/api/midleware/schedulde.js
@@ -7,7 +7,7 @@ var value = false;
 console.log("schedule1");
 
 function schedule1(min, hour, value) {
-        console.log(min + " " + hour + " " + state);
+        console.log(min + " " + hour + " " + value);
         var j = schedule.scheduleJob('' + min + ' ' + hour + ' * * *', function(){
         console.log("schedule");
         leds.led(21, value);
