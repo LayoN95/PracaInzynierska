c870c6432421ce9db5845380d36d3bd436dedba5
LayoN95 <mborowczyk95@gmail.com>
2019-07-24 16:44:50 +0200

    update

--- Server/api/midleware/schedulde.js
+++ Server/api/midleware/schedulde.js
@@ -1,7 +1,8 @@
 const leds = require('./leds');
 var schedule = require('node-schedule');
 var date = new Date(2019, 6, 24, 15, 52, 0);
-var min, hour, state = 0;
+var min, hour = 0;
+var state;
 
 console.log("schedule1");
 
@@ -17,3 +18,4 @@ function schedule1(min, hour, state) {
   module.exports.schedule1 = schedule1;
   module.exports.min = min;
   module.exports.hour = hour;
+  module.exports.state = state;
