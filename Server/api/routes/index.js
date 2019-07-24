3c8966b18c17aa658b322e91b0c0c4a173e9259f
LayoN95 <mborowczyk95@gmail.com>
2019-07-24 16:28:59 +0200

    update

--- Server/api/routes/index.js
+++ Server/api/routes/index.js
@@ -102,7 +102,7 @@ router.post('/leds/:ledId/:value', (req, res, next) => {
   }
 });
 
-router.post('/schedulde/:hour/:min'), (req, res, next) => {
+router.post('/schedulde/:hour/:min', (req, res, next) => {
  /* //req.params.hour = schedulde.hour;
   //req.params.min = schedulde.min;
   console.log("hour:" + req.params.hour + " min: " + req.params.min);
@@ -110,6 +110,8 @@ router.post('/schedulde/:hour/:min'), (req, res, next) => {
   res.status(200).json({
     message: ("Submit")
   });
-}
+});
+
+
 
 module.exports = router;
