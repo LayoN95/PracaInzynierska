1435349845e6a7f72795afda48de1e38f6c80911
LayoN95 <mborowczyk95@gmail.com>
2019-07-25 15:24:54 +0200

    update

--- Server/api/midleware/ds18b20.js
+++ Server/api/midleware/ds18b20.js
@@ -14,11 +14,11 @@ setInterval(function () {
             console.log(err);
             console.log("Brak urządzenia");
         } else {
-            const DS18B20schema = ds18b20Schema({
+            /*const DS18B20schema = ds18b20Schema({
                 _id: mongoose.Types.ObjectId(),
                 temperature: temperature
             });
-            DS18B20schema.save();
+            DS18B20schema.save();*/
 
             console.log("temperature" + temperature);
             temp = temperature;
@@ -31,8 +31,12 @@ setInterval(function () {
  
 var rule = new schedule.RecurrenceRule();
 
-rule.minute = 15;
+rule.minute = [0,15,30,45];
  
 var j = schedule.scheduleJob(rule, function(){
-  console.log('Today is recognized by Rebecca Black!');
+    const DS18B20schema = ds18b20Schema({
+        _id: mongoose.Types.ObjectId(),
+        temperature: temp
+    });
+    DS18B20schema.save();
 });
