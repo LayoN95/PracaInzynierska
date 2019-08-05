4ed8f8a9516ec39e6b4d204e87d67bfa3a3f3824
LayoN95 <mborowczyk95@gmail.com>
2019-08-05 17:04:05 +0200

    update

--- Server/api/midleware/ds18b20.js
+++ Server/api/midleware/ds18b20.js
@@ -1,6 +1,7 @@
 const ds18b20 = require('ds18b20-raspi');
 const ds18b20Schema = require('../models/ds18b20');
 const mongoose = require('mongoose');
+
 var temp = 0;
 
 var schedule = require('node-schedule');
@@ -14,11 +15,7 @@ setInterval(function () {
             console.log(err);
             console.log("Brak urządzenia");
         } else {
-            const DS18B20schema = ds18b20Schema({
-                _id: mongoose.Types.ObjectId(),
-                temperature: temperature
-            });
-            DS18B20schema.save();
+
 
             console.log("temperature" + temperature);
             temp = temperature;
@@ -31,9 +28,13 @@ setInterval(function () {
  
 var rule = new schedule.RecurrenceRule();
 
-rule.minute = [0,15,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,45];
+rule.minute = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,57,58,59];
  
 var j = schedule.scheduleJob(rule, function(){
   console.log('Today is recognized by Rebecca Black!');
-
+  const DS18B20schema = ds18b20Schema({
+    _id: mongoose.Types.ObjectId(),
+    temperature: temp
+});
+DS18B20schema.save();  
 });
