f29d79d0be08566f1791ac37e2e1a540381891df
LayoN95 <mborowczyk95@gmail.com>
2019-08-05 16:56:55 +0200

    update

--- Server/api/midleware/dht11.js
+++ Server/api/midleware/dht11.js
@@ -6,6 +6,7 @@ var schedule = require('node-schedule');
 
 var temp = 0;
 var humid = 0;
+var tem;
  
 setInterval(function () {
 
@@ -17,9 +18,11 @@ sensor.read(11, 26, function(err, temperature, humidity) {
             humidity: humidity
         });
         DHT11schema.save();*/
+        tem = temperature;
+        console.log("tem = " + tem);
         this.temp = temperature.toFixed(1);
         this.humid = humidity.toFixed(1);
-        console.log(temp + " " + humid); 
+        console.log(this.temp + " " + this.humid); 
         //module.exports.temp = temperature;
         //module.exports.humid = humidity;
         console.log('temp: ' + temperature.toFixed(1) + '°C, ' +
@@ -31,7 +34,7 @@ sensor.read(11, 26, function(err, temperature, humidity) {
 
 var rule = new schedule.RecurrenceRule();
 
-rule.minute = [0,15,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,57,58,59];
+rule.minute = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,57,58,59];
  
 var j = schedule.scheduleJob(rule, function(){
     
