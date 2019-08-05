f347494dd99b6ec036392c8a4c75855e7509206e
LayoN95 <mborowczyk95@gmail.com>
2019-08-05 16:39:34 +0200

    update

--- Server/api/midleware/dht11.js
+++ Server/api/midleware/dht11.js
@@ -30,7 +30,7 @@ sensor.read(11, 26, function(err, temperature, humidity) {
 
 var rule = new schedule.RecurrenceRule();
 
-rule.minute = [0,15,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,45];
+rule.minute = [0,15,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,57,58,59];
  
 var j = schedule.scheduleJob(rule, function(){
     
@@ -42,4 +42,6 @@ var j = schedule.scheduleJob(rule, function(){
     DHT11schema.save();
     console.log("DHT11 Zapisano!" + temp + " " + humid);
     console.log(`${this.temp}`);
+    console.log(`${temp}`);
+
 });
