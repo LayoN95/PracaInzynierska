18abfd96dd3409c8a360176e5775747742d7582a
LayoN95 <mborowczyk95@gmail.com>
2019-08-20 20:41:26 +0200

    update

--- Server/api/midleware/thermostat.js
+++ Server/api/midleware/thermostat.js
@@ -6,10 +6,12 @@ var temp;
 setInterval(function () {
 console.log(`DS18B20 Temperature ${ds18b20.temp}`);
 console.log(`Thermostat SET temperature ${temp}`);
+
+module.exports.temp = temp;
+
 if (ds18b20.temp < temp) {
     console.log("Turn on the heater.");
 } else if (ds18b20.temp >= temp) {
     console.log("Turn off the heater");
 }}, 15000);
 
-module.exports.temp = temp;
