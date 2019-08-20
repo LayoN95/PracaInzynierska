f09888d867c9f0eb6f30c0fe8efd112e0fb8031f
LayoN95 <mborowczyk95@gmail.com>
2019-08-20 20:48:14 +0200

    update

--- Server/api/midleware/thermostat.js
+++ Server/api/midleware/thermostat.js
@@ -4,6 +4,7 @@ const ds18b20 = require('./ds18b20');
 var temp;
 function setTemp(setTemp){
     temp = setTemp;
+    console.log("Temp after setTemp()" + temp);
 }
 setInterval(function () {
 console.log(`DS18B20 Temperature ${ds18b20.temp}`);
