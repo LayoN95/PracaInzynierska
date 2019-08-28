8dd25c8a32c7b5f9cc3b58c45fde1c5b492887ca
LayoN95 <mborowczyk95@gmail.com>
2019-08-28 19:08:48 +0200

    update

--- Client/src/layout/dashboard/DashboardLayout.vue
+++ Client/src/layout/dashboard/DashboardLayout.vue
@@ -4,7 +4,7 @@
       <template slot="links">
         <sidebar-link to="/dashboard" :name="$t('sidebar.dashboard')" icon="tim-icons icon-chart-pie-36"/>
         <sidebar-link to="/icons" :name="$t('sidebar.icons')" icon="tim-icons icon-atom"/>
-        <sidebar-link to="/maps" :name="$t('sidebar.control')" icon="tim-icons icon-pin"/>
+        <sidebar-link to="/control" :name="$t('sidebar.control')" icon="tim-icons icon-pin"/>
         <sidebar-link to="/notifications" :name="$t('sidebar.notifications')" icon="tim-icons icon-bell-55"/>
         <sidebar-link to="/profile" :name="$t('sidebar.userProfile')" icon="tim-icons icon-single-02"/>
         <sidebar-link to="/table-list" :name="$t('sidebar.tableList')" icon="tim-icons icon-puzzle-10"/>
