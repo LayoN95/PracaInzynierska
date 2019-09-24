2c5541e5095be45b46f3d28d0b3df0fd55848eae
LayoN95 <mborowczyk95@gmail.com>
2019-09-24 19:39:09 +0200

    update

--- Server/api/models/devicesStatus.js
+++ Server/api/models/devicesStatus.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const devicesSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    name: {type: String, required: true},
     state: {type: Boolean, required: true},
     date: {type: Date, default: Date.now }
 });
