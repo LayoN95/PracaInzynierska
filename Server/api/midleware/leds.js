84d88868ef16fb10e067918d897cbf0bc8535fbf
LayoN95 <mborowczyk95@gmail.com>
2019-09-24 20:14:32 +0200

    update

--- Server/api/midleware/leds.js
+++ Server/api/midleware/leds.js
@@ -7,14 +7,20 @@ const devicesSchema = require('../models/devicesStatus');
     function led(ledId, value) {
         console.log("FUNKCJA DZIALA " + ledId + " " + value);
         var LED = new Gpio(ledId, 'out'); 
-        devicesSchema.findById('5d8a5b771c9d4400002e3fa9', function(err, doc) {
+        /*devicesSchema.findById('5d8a5b771c9d4400002e3fa9', function(err, doc) {
             if (err) {
                 console.log("erorr not found");
             }
             doc.state = value;
             doc.save();
-        })
+        })*/
+        const DEVICEstatus = devicesSchema({
+            _id: mongoose.Types.ObjectId(),
+            name: "light2",
+            state: value
 
+        });
+        DEVICEstatus.save();
 
         LED.writeSync(value);
 
