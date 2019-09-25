bf0a5b64872a2cc3c247f61622d7fe51f03f6b62
LayoN95 <mborowczyk95@gmail.com>
2019-09-25 18:47:02 +0200

    update

--- Client/src/pages/Icons.vue
+++ Client/src/pages/Icons.vue
@@ -1,11 +1,12 @@
 <template>
     <div class="row">
       <div class="col-12">
-
+123
       </div>
 
       <div class="col-12">
         <card class="card-plain">
+          123
           <!--<div class="table-full-width table-responsive">
             <base-table :title="table2.title" :sub-title="table2.subTitle" :data="table2.data"
                          :columns="table2.columns">
