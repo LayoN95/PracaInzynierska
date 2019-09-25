440b0a0f72a96268eefe4db2245e0ed1d27c0d21
LayoN95 <mborowczyk95@gmail.com>
2019-09-25 18:55:32 +0200

    update

--- Client/src/pages/Icons.vue
+++ Client/src/pages/Icons.vue
@@ -2,7 +2,7 @@
 <div class="row">
       <div class="col-lg-4">
         <card>
-          <h4 slot="header">Light 1</h4>
+          <h4 slot="header">Światło</h4>
 
         </card>
       </div>
@@ -14,29 +14,29 @@
       </div>
       <div class="col-lg-4">
         <card>
-          <h4 slot="header">Schedule</h4>
+          <h4 slot="header">Alarm</h4>
 
 
         </card>
       </div>
         <div class="col-lg-4">
         <card>
-           <h4 slot="header">Schedule</h4>
+           <h4 slot="header">Temperatura</h4>
         </card>
       </div>
         <div class="col-lg-4">
         <card>
-           <h4 slot="header">Schedule</h4>
+           <h4 slot="header">Pir</h4>
         </card>
       </div>
         <div class="col-lg-4">
         <card>
-           <h4 slot="header">Schedule</h4>
+           <h4 slot="header">HCSR</h4>
         </card>
       </div>
       <div class="col-12">
         <card>
-           <h4 slot="header">Schedule</h4>
+           <h4 slot="header">Okna</h4>
         </card>
       </div>
 </div>
