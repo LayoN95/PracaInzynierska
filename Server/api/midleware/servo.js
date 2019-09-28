04a88ff482e06f9be61a08b96ce2e5dc5a10e328
LayoN95 <mborowczyk95@gmail.com>
2019-09-28 13:56:31 +0200

    update

--- Server/api/midleware/servo.js
+++ Server/api/midleware/servo.js
@@ -1,4 +1,6 @@
 const Gpio = require('pigpio').Gpio;
+const mongoose = require('mongoose');
+const devicesSchema = require('../models/devicesStatus');
  
 const motor = new Gpio(13, {mode: Gpio.OUTPUT});
  
@@ -6,6 +8,14 @@ let pulseWidth = 1550;
 
 function servoControl(value) {
     motor.servoWrite(value);
+
+    devicesSchema.findById('5d8a5d38456fa304cebf8f4a', function(err, doc) {
+      if (err) {
+          console.log("erorr not found");
+      }
+      doc.window_open = value;
+      doc.save();
+  })
 }
 
 /*setInterval(() => {
--- Server/api/models/devicesStatus.js
+++ Server/api/models/devicesStatus.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const devicesSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     state: {type: Boolean, required: true},
+    window_open: {type: String, required: true},
     date: {type: Date, default: Date.now }
 });
 
