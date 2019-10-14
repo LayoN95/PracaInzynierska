3b725a4c61ac23f56d15664e7606df596965d95d
LayoN95 <mborowczyk95@gmail.com>
2019-10-14 18:39:49 +0200

    update

--- Client/src/pages/Icons.vue
+++ Client/src/pages/Icons.vue
@@ -62,7 +62,7 @@
         <card>
            <h4 slot="header"><i class="tim-icons icon-image-02 text-success "></i> Okna</h4>
            <p>Stan okna: {{ window }}</p>
-            <progress-bar :val="temperature" :text="temperature" max="2500"></progress-bar>
+            <progress-bar :val="humidity" :text="temperature" max="2500"></progress-bar>
 
               <vue-simple-progress size="large" :val="temperature" :text="temperature"></vue-simple-progress>
         </card>
