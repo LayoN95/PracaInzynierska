f954cf9b82e13514989bb862a5bbb218038b3ea2
LayoN95 <mborowczyk95@gmail.com>
2019-10-21 18:45:59 +0200

    update

--- Server/api/midleware/thermostat.js
+++ Server/api/midleware/thermostat.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const ds18b20 = require('./ds18b20');
+const Gpio = require('onoff').Gpio;
+var RELAY = new Gpio(25, 'out');
 
 var temp;
 function setTemp(setTemp){
@@ -12,8 +14,11 @@ console.log(`Thermostat SET temperature ${temp}`);
 
 if (ds18b20.temp < temp) {
     console.log("Turn on the heater.");
+    RELAY.writeSync(0);
 } else if (ds18b20.temp >= temp) {
     console.log("Turn off the heater");
+    RELAY.writeSync(1);
+
 }}, 60000);
 
 module.exports.setTemp = setTemp;
