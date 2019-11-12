1092b71cb197aae531b731c04597b8942653fe55
LayoN95 <mborowczyk95@gmail.com>
2019-11-12 17:19:21 +0100

    update

--- Client/src/pages/TableList.vue
+++ Client/src/pages/TableList.vue
@@ -31,58 +31,8 @@ import { BaseTable } from "@/components";
   import axios from 'axios';
 
 var table = [];
-const tableColumns = ["temperature", "humidity", "date", "id"];
-const tableData = [
-  {
-    id: 1,
-    name: "Dakota Rice",
-    salary: "$36.738",
-    country: "Niger",
-    city: "Oud-Turnhout",
-  },
-  {
-    id: 2,
-    name: "Minerva Hooper",
-    salary: "$23,789",
-    country: "Curaçao",
-    city: "Sinaai-Waas"
-  },
-  {
-    id: 3,
-    name: "Sage Rodriguez",
-    salary: "$56,142",
-    country: "Netherlands",
-    city: "Baileux"
-  },
-  {
-    id: 4,
-    name: "Philip Chaney",
-    salary: "$38,735",
-    country: "Korea, South",
-    city: "Overland Park"
-  },
-  {
-    id: 5,
-    name: "Doris Greene",
-    salary: "$63,542",
-    country: "Malawi",
-    city: "Feldkirchen in Kärnten"
-  },
-  {
-    id: 6,
-    name: 'Mason Porter',
-    salary: '$98,615',
-    country: 'Chile',
-    city: 'Gloucester'
-  },
-  {
-    id: 7,
-    name: 'Jon Porter',
-    salary: '$78,615',
-    country: 'Portugal',
-    city: 'Gloucester'
-  }
-]; 
+const tableColumns = ["temperature", "humidity", "date"];
+ 
 
 export default {
   components: {
@@ -92,19 +42,8 @@ export default {
 
     return {
       
-      /*table1: {
-        title: "Simple Table",
-        columns: [...tableColumns],
-        data: [...tableData]
-      },
-      table2: {
-        title: "Table on Plain Background",
-        columns: [...tableColumns],
-        data: [...tableData]
-      }*/
-      
       table1: {
-        title: "Simple Table:",
+        title: "DHT 11 Table:",
         columns: [...tableColumns],
         data: [...table]
       }
