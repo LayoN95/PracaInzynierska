a3065f54f916f11ceb08253611013b8476d69bcd
LayoN95 <mborowczyk95@gmail.com>
2019-11-18 19:28:07 +0100

    update

--- Server/api/midleware/bh1750.js
+++ Server/api/midleware/bh1750.js
@@ -5,15 +5,16 @@ var light = new BH1750({
     command: 0x10,
     length: 2
 });
-
+var lightRead;
 console.log("BH1750");
- setInterval(function lightRead() {
+ setInterval(function() {
 light.readLight(function(err, value){
     if (err) {
         console.log("light error: " + err);
         throw err;
     } else {
         console.log("light value is: ", value, "lx");
+        lightRead = value;
     }
 });
 }, 15000);
