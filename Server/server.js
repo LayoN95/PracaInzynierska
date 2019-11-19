43c7d12639e477ee146df3509dc252f1c48723a8
LayoN95 <mborowczyk95@gmail.com>
2019-11-19 11:48:03 +0100

    update

--- Server/server.js
+++ Server/server.js
@@ -5,5 +5,17 @@ const app = require('./app');
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
+
+//SOCKET.IO
+var io = require('socket.io')(server);
+
 server.listen(port);
 
+io.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+      console.log(data);
+    });
+  });
+
+  //=======
