09c702aa86b637a3c9409062cb6b646f8df843a9
LayoN95 <mborowczyk95@gmail.com>
2019-11-19 14:46:13 +0100

    update

--- Server/server.js
+++ Server/server.js
@@ -7,6 +7,6 @@ const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 //server.listen(port);
 var serverListen = app.listen(port);
-console.log("Serwer działa na porcie " + server.address().address);
+console.log("Serwer działa na porcie " + serverListen.address().port);
 
 module.exports.serverListen = serverListen;
