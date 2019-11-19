a8e0bb2a4596005d4aaa46775176ac89d8e3169e
LayoN95 <mborowczyk95@gmail.com>
2019-11-19 13:46:12 +0100

    update

--- Server/app.js
+++ Server/app.js
@@ -10,7 +10,7 @@ const mongoose = require('mongoose');
 
 // CORS SETTINGS
 const cors = require('cors');
-
+/*
 var whitelist = ['http://192.168.1.48:8081', 'http://192.168/1/48:3000']
 var corsOptions = {
   origin: function (origin, callback) {
@@ -21,7 +21,7 @@ var corsOptions = {
     }
   }
 }
-
+*/
 //================================
 
 var indexRouter = require('./api/routes/index');
@@ -36,12 +36,28 @@ process.env.MONGO_ATLAS_PW +
 
 mongoose.Promise = global.Promise;
 var app = express();
-app.use(cors(corsOptions));
+app.use(cors({origin: '*'}));
+app.use(function (req,res, next) {
+     // Website you wish to allow to connect
+     res.header('Access-Control-Allow-Origin', '*');
 
+     // Request methods you wish to allow
+     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+ 
+     // Request headers you wish to allow
+     res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+ 
+     // Set to true if you need the website to include cookies in the requests sent
+     // to the API (e.g. in case you use sessions)
+     res.setHeader('Access-Control-Allow-Credentials', false);
+ 
+     // Pass to next layer of middleware
+     next(); 
+})
 //SOCKET.IO
 var io = require('socket.io').listen(ServerIO.server);
 //io.set('origins', '*:*');
-io.set('Access-Control-Allow-Credentials: true', 'origins', 'http://192.168.1.48:8081');
+io.set('origins', 'http://192.168.1.48:8081');
 
 io.on('connection', function(socket) {
   console.log("SOCKET.IO DZIAŁA");
