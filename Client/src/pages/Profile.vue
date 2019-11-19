a1206249eabe977fc3937eb805abcddebda92a26
LayoN95 <mborowczyk95@gmail.com>
2019-11-19 15:37:31 +0100

    update

--- Client/src/pages/Profile.vue
+++ Client/src/pages/Profile.vue
@@ -70,7 +70,7 @@ Vue.use(new VueSocketIO({
       customEmit: function (data) {
         console.log('this method was fired by the socket server eg: io.');
       },
-      bh1750: function (data) {
+      BH1750: function (data) {
         console.log(data);
       }
     },
