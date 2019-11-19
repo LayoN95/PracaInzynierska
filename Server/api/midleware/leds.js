69681f5472da53ef94a074d8dfc16eff3d64bbbe
LayoN95 <mborowczyk95@gmail.com>
2019-11-19 17:43:11 +0100

    update

--- Server/api/midleware/leds.js
+++ Server/api/midleware/leds.js
@@ -18,19 +18,19 @@ const devicesSchema = require('../models/devicesStatus');
         switch(ledId) {
             case 18: 
             {
-              console.log("Case 18: ROOM 1");
+              console.log("inside LED Case 18: ROOM 1");
               socket.emit('LED_18', {state: value});
               break;
             }
             case 6: 
             {
-              console.log("Case 6: ROOM 2");
+              console.log("inside LED Case 6: ROOM 2");
               socket.emit('LED_6', {state: value});  
               break;
             }
             case 21: 
             {
-              console.log("Case 21: OUTDOOR");
+              console.log("inside LED Case 21: OUTDOOR");
               socket.emit('LED_21', {state: value});
               break;
             }
