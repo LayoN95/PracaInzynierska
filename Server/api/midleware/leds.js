422fa418bf195d8b09edcaa3afe8788e484b9594
LayoN95 <mborowczyk95@gmail.com>
2019-11-19 17:47:15 +0100

    update

--- Server/api/midleware/leds.js
+++ Server/api/midleware/leds.js
@@ -2,19 +2,7 @@ const Gpio = require('onoff').Gpio;
 const mongoose = require('mongoose');
 const devicesSchema = require('../models/devicesStatus');
 
-    var value = false;
-    var ledId = 0;
     function led(ledId, value) {
-        console.log("FUNKCJA DZIALA " + ledId + " " + value);
-        var LED = new Gpio(ledId, 'out'); 
-        devicesSchema.findById('5d8a5d38456fa304cebf8f4a', function(err, doc) {
-            if (err) {
-                console.log("erorr not found");
-            }
-            doc.state = value;
-            doc.save();
-        })
-
         switch(ledId) {
             case 18: 
             {
@@ -35,6 +23,17 @@ const devicesSchema = require('../models/devicesStatus');
               break;
             }
           }
+        console.log("FUNKCJA DZIALA " + ledId + " " + value);
+        var LED = new Gpio(ledId, 'out'); 
+        devicesSchema.findById('5d8a5d38456fa304cebf8f4a', function(err, doc) {
+            if (err) {
+                console.log("erorr not found");
+            }
+            doc.state = value;
+            doc.save();
+        })
+
+
           LED.writeSync(value);
 
 
