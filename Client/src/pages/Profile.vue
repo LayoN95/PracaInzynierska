69c204fdd175d1f0b8be318aaf05f7483c7a92e9
LayoN95 <mborowczyk95@gmail.com>
2019-11-20 14:24:45 +0100

    update

--- Client/src/pages/Profile.vue
+++ Client/src/pages/Profile.vue
@@ -1,15 +1,10 @@
 <template>
   <div class="row">
-    <div class="col-md-8">
-      <edit-profile-form :model="model">
-      </edit-profile-form>
+    <div class="col-md-12">
+      <img :src="camera_picture">
     </div>
-    <div class="col-md-4">
-      <user-card :user="user"></user-card>
-    </div>
-    <div class="col-md-8">
-      <h1>SOCKET.IO</h1>
-    </div>
+
+
   </div>
 
 </template>
@@ -20,6 +15,8 @@
 <script>
   import EditProfileForm from './Profile/EditProfileForm';
   import UserCard from './Profile/UserCard';
+  import Vue from 'vue';
+
 
   /*
   import Vue from 'vue';
@@ -39,30 +36,22 @@ Vue.use(new VueSocketIO({
     data() {
       return {
         //socket : io('http://192.168.1.48:3000'),
-
-        model: {
-          company: 'Creative Code Inc.',
-          email: 'mike@email.com',
-          username: 'michael23',
-          firstName: 'Mike',
-          lastName: 'Andrew',
-          address: 'Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09',
-          city: 'Melbourne',
-          country: 'Australia',
-          about: 'Lamborghini Mercy, Your chick she so thirsty, I\'m in that two seat Lambo.'
-        },
-        user: {
-          fullName: 'Mike Andrew',
-          title: 'Ceo/Co-Founder',
-          description: `Do not be scared of the truth because we need to restart the human foundation in truth And I love you like Kanye loves Kanye I love Rick Owens’ bed design but the back is...`,
-        }
+        camera_picture: 'http://192.168.1.152:8080/shot.jpg'
       }
     },
 
     methods: {
+          
+        getCamera: function (event) {
+          setInterval(function () {
+          this.camera_picture = 'http://192.168.1.152:8080/shot.jpg';
+          }, 1000);
+        
 
+      }
     },
     mounted(){
+      this.getCamera();
     },
 
     beforeMount(){
