9019c3b6c4fc7e23569fa522ef97ed0bb7ecae8c
LayoN95 <mborowczyk95@gmail.com>
2019-11-20 14:37:02 +0100

    update

--- Server/api/midleware/thermostat.js
+++ Server/api/midleware/thermostat.js
@@ -16,14 +16,49 @@ console.log(`Thermostat SET temperature ${temp}`);
 if (ds18b20.temp < temp) {
     console.log("Turn on the heater.");
     socket.emit('heater_turn_on', {value: true});
+    
+    DEVICE_STATUS.findById('5d8a5d38456fa304cebf8f4a', function(err, doc) {
+        if (err) {
+            console.log("erorr not found");
+        }
+        doc.radiator = true;
+        doc.save();
+       })
+
 } else if (ds18b20.temp >= temp) {
     console.log("Turn off the heater");
     console.log("turn air conditioner off");
     socket.emit('heater_turn_off', {value: false});
     socket.emit('air_conditioner_turn_off', {value: false});
+
+    DEVICE_STATUS.findById('5d8a5d38456fa304cebf8f4a', function(err, doc) {
+        if (err) {
+            console.log("erorr not found");
+        }
+        doc.air_conditioner = false;
+        doc.save();
+       })
+
+    DEVICE_STATUS.findById('5d8a5d38456fa304cebf8f4a', function(err, doc) {
+        if (err) {
+            console.log("erorr not found");
+        }
+        doc.radiator = false;
+        doc.save();
+       })
+
 } else if (ds18b20.temp > temp){
     console.log("Turn air conditioner on");
     socket.emit('air_conditioner_turn_on', {value: true});
+    
+    DEVICE_STATUS.findById('5d8a5d38456fa304cebf8f4a', function(err, doc) {
+        if (err) {
+            console.log("erorr not found");
+        }
+        doc.air_conditioner = true;
+        doc.save();
+       })
+
 }}, 60000);
 
 module.exports.setTemp = setTemp;
--- Server/api/routes/index.js
+++ Server/api/routes/index.js
@@ -248,6 +248,7 @@ router.get('/devicestatus', (req, res, next) => {
 
 router.post('/airconditioner/:value', (req,res,next) => {
   airConditioner.airConditioner(req.params.value);
+
   res.status(200).json({
     message: (req.params.value)
   })
