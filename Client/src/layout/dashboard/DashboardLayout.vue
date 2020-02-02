d7dbaaa399a064c53eff60b9e983200d05f7d078
LayoN95 <43011917+LayoN95@users.noreply.github.com>
2020-02-02 13:00:16 +0100

    up

--- Client/src/layout/dashboard/DashboardLayout.vue
+++ Client/src/layout/dashboard/DashboardLayout.vue
@@ -6,8 +6,8 @@
         <sidebar-link to="/zestawienie" :name="$t('sidebar.zestawienie')" icon="tim-icons icon-atom"/>
         <sidebar-link to="/sterowanie" :name="$t('sidebar.sterowanie')" icon="tim-icons icon-pin"/>
         <sidebar-link to="/wizualizacja" :name="$t('sidebar.wizualizacja')" icon="tim-icons icon-bell-55"/>
-        <sidebar-link to="/profile" :name="$t('sidebar.userProfile')" icon="tim-icons icon-single-02"/>
-        <sidebar-link to="/table-list" :name="$t('sidebar.tableList')" icon="tim-icons icon-puzzle-10"/>
+        <sidebar-link to="/kamera" :name="$t('sidebar.kamera')" icon="tim-icons icon-single-02"/>
+        <sidebar-link to="/historia" :name="$t('sidebar.kamera')" icon="tim-icons icon-puzzle-10"/>
        <!-- <sidebar-link to="/typography" :name="$t('sidebar.typography')" icon="tim-icons icon-align-center"/> -->
        <!-- <sidebar-link to="/dashboard?enableRTL=true" :name="$t('sidebar.rtlSupport')" icon="tim-icons icon-world"/> -->
       </template>
