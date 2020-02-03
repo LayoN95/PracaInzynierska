594a1cae62272af6f9b85c04952005427cb0c071
LayoN95 <43011917+LayoN95@users.noreply.github.com>
2020-02-03 18:49:46 +0100

    Update index.js

--- Server/api/routes/index.js
+++ Server/api/routes/index.js
@@ -9,7 +9,7 @@ const DEVICE_STATUS = require("../models/devicesStatus");
 
 //const schedule = require("../midleware/schedule");
 const ds18b20 = require("../midleware/ds18b20");
-//const dht11 = require("../midleware/dht11");
+const dht11 = require("../midleware/dht11");
 const pir = require("../midleware/pirHCSR501");
 const leds = require("../midleware/leds");
 const thermostat = require("../midleware/thermostat");
