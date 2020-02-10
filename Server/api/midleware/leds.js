22833981b6a8859e3cc123273918660d3d16b7c8
LayoN95 <43011917+LayoN95@users.noreply.github.com>
2020-02-10 22:21:50 +0100

    Update leds.js

--- Server/api/midleware/leds.js
+++ Server/api/midleware/leds.js
@@ -4,10 +4,6 @@ const devicesSchema = require("../models/devicesStatus");
 var socket = require("socket.io-client")("http://192.168.1.48:3000");
 
 function led(ledId, value) {
-  //console.log("FUNKCJA DZIALA " + ledId + " " + value);
-
-  var check_led = ledId;
-  //console.log("CHECK_KED: " + check_led);
   switch (ledId) {
     case "18": {
       //console.log("inside LED Case 18: ROOM 1");
@@ -25,7 +21,6 @@ function led(ledId, value) {
       break;
     }
   }
-
   var LED = new Gpio(ledId, "out");
   devicesSchema.findById("5d8a5d38456fa304cebf8f4a", function(err, doc) {
     if (err) {
