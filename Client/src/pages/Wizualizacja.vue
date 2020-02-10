4fd17e029d88efdff87be71101b2dd81203d4f91
LayoN95 <43011917+LayoN95@users.noreply.github.com>
2020-02-10 20:15:22 +0100

    Update Wizualizacja.vue

--- Client/src/pages/Wizualizacja.vue
+++ Client/src/pages/Wizualizacja.vue
@@ -53,14 +53,14 @@
 
           <div
             class="light_1"
-            style="position: absolute; top: 450px; left: 600px;"
+            style="position: absolute; top: 450px; left: 620px;"
           >
             <img :src="lightRoom_1" style="width: 32px;" />
           </div>
 
           <div
             class="light_2"
-            style="position: absolute; top: 450px; left: 350px;"
+            style="position: absolute; top: 450px; left: 320px;"
           >
             <img :src="lightRoom_2" style="width: 32px;" />
           </div>
