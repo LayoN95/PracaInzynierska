512867c1d08666021385754c0f7f4982ca461fdf
LayoN95 <43011917+LayoN95@users.noreply.github.com>
2020-02-10 21:42:04 +0100

    Update bh1750.js

--- Server/api/midleware/bh1750.js
+++ Server/api/midleware/bh1750.js
@@ -2,7 +2,7 @@ var BH1750 = require("bh1750");
 var socket = require("socket.io-client")("http://192.168.1.48:3000");
 var Gpio = require("onoff").Gpio;
 var LIGHT = new Gpio(17, 'out');
-var LED =  require("leds");
+var LED =  require("../midleware/leds");
 
 var light = new BH1750({
   address: 0x23,
